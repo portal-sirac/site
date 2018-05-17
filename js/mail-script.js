3c30c94678ae23d078af5a0ab429c84d37cda687
LuisPortugal <luisenriquegomesportugal@gmail.com>
2018-05-16 23:36:09 -0300

    teste header

--- js/mail-script.js
+++ js/mail-script.js
@@ -1,31 +1,18 @@
-    // -------   Mail Send ajax
+firebase
+    .initializeApp({
+    apiKey: "OsjXcKscVnhEGJyJ3HVAWsOKkykw5LQQKg9bq7bz",
+    authDomain: "sirac-f1bda.firebaseapp.com",
+    databaseURL: "https://sirac-f1bda.firebaseio.com"
+});
 
-     $(document).ready(function() {
-        var form = $('#myForm'); // contact form
-        var submit = $('.submit-btn'); // submit button
-        var alert = $('.alert-msg'); // alert div for show alert message
-
-        // form submit event
-        form.on('submit', function(e) {
-            e.preventDefault(); // prevent default form submit
-
-            $.ajax({
-                url: 'mail.php', // form action url
-                type: 'POST', // form submit method get/post
-                dataType: 'html', // request type html/json/xml
-                data: form.serialize(), // serialize form data
-                beforeSend: function() {
-                    alert.fadeOut();
-                    submit.html('Sending....'); // change submit button text
-                },
-                success: function(data) {
-                    alert.html(data).fadeIn(); // fade in response data
-                    form.trigger('reset'); // reset form
-                    submit.attr("style", "display: none !important");; // reset submit button text
-                },
-                error: function(e) {
-                    console.log(e)
-                }
-            });
-        });
+$(document).ready(function() {
+    $("#newsletters_emails_form").on("submit", function(e){
+        e.preventDefault();
+        firebase.database().ref('site/newsletters_emails')
+            .push()
+            .set(
+            {
+                email: $("#newsletters_emails_form input[name=email]").val()
+            }); 
     });
+});
